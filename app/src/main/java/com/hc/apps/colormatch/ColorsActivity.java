95ffad1ccfa2bf44a80aa14208199f398efa3547
harshhc <harshchandrahc5@gmail.com>
2019-11-17 12:10:06 +0000

    initial commit

---
+++ app/src/main/java/com/hc/apps/colormatch/ColorsActivity.java
@@ -0,0 +1,40 @@
+package com.hc.apps.colormatch;
+
+import androidx.appcompat.app.AppCompatActivity;
+import in.mayanknagwanshi.imagepicker.ImageSelectActivity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.widget.ImageView;
+
+
+public class ColorsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_colors);
+
+        Intent intent = new Intent(this, ImageSelectActivity.class);
+        intent.putExtra(ImageSelectActivity.FLAG_COMPRESS, false);//default is true
+        intent.putExtra(ImageSelectActivity.FLAG_CAMERA, true);//default is true
+        intent.putExtra(ImageSelectActivity.FLAG_GALLERY, false);//default is true
+        startActivityForResult(intent, 1213);
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == 1213 && resultCode == Activity.RESULT_OK) {
+            String filePath = data.getStringExtra(ImageSelectActivity.RESULT_FILE_PATH);
+            Bitmap selectedImage = BitmapFactory.decodeFile(filePath);
+
+            ImageView img= findViewById(R.id.image);
+            img.setImageBitmap(selectedImage);
+        }
+    }
+}
